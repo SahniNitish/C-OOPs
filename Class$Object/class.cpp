5062df89662c415b77a905fecbc21dd2ef35e355
Nitish sahni <nitish@realmeta.ca>
2025-11-02 13:00:14 -0400

    Added class C++ files

---
+++ Class$Object/class.cpp
@@ -0,0 +1,10 @@
+//creating a class
+
+#include <iostream>
+using namespace std;
+
+class Myclass{       //The class
+    public:          //Access specifier
+    int myNum;       //Attribute (int variable)
+    string myString; //Attribute (string variable)
+}
---
+++ Class$Object/object.cpp
@@ -0,0 +1,13 @@
+//Create a an object "myObj" and acccess the attributes:
+#include <iostream>
+using namespace std;
+
+class Myclass{       //The class
+    public:          //Access specifier
+    int myNum;       //Attribute (int variable)
+    string myString; //Attribute (string variable)
+}
+
+int main (){
+    Myclass myObj: //create an object of Myclass
+}
