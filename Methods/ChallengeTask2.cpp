4914ff19531af802868d57e5330252234d1afd08
Nitish sahni <nitish@realmeta.ca>
2025-12-08 14:02:17 -0400

    methods

--- Methods/ChallengeTask2.cpp
+++ Methods/ChallengeTask2.cpp
@@ -3,22 +3,23 @@ Create a class Dog with method bark() that print "woof!".
 then call that method from the main() using an object of the class. 
 
 */
-
 #include <iostream>
-using namespace std; 
+using namespace std;
 
-class Dog{
-  public:
-       void bark();
-
+class cat{
+  
+    public:
+  void meaoo();
 };
 
-void Dog:: bark(){
-    cout << "Woof!";
+void cat::meaoo(){
+    cout << "mao mao"
 }
 
-int  main(){
-    Dog myObj;
-    myObj.bark();
+
+  
+   int main(){
+    cat myObj;
+    cout << myObj.meaoo();
     return 0 ; 
-}
+   }
